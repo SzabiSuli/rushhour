c6490d27e0bf0912cac4b7933c4715eaf1d9d0ae
SzecsiSzabi <szabolcs.szecsi@optimaze.hu>
2026-03-05 10:30:02 +0100

    code cleanup

--- src/graphedge.cs
+++
@@ -1,9 +0,0 @@
-namespace rushhour.src;
-
-using rushhour.src.Model;
-
-public class GraphEdge {
-    public GraphNode From { get; set; }
-    public GraphNode To { get; set; }
-    public Move MoveUsed { get; set; }
-}
